61855daa915837c780d9c990697f3acbc158c5de
JhoanVanDam <jhoansteam@gmail.com>
2023-06-11 12:48:46 -0500

    ajusted de logo

--- src/Components/about/about.jsx
+++ src/Components/about/about.jsx
@@ -1,10 +1,16 @@
 import React, { Component } from "react";
 import style from "./about.css";
+import jhoan from "./jhoan.jpg";
 class about extends Component {
     render() {
         return (
             <div className="about-main">
-                <p>ola</p>
+                <div className="about-img">
+                    <img src={jhoan} alt="logo" />
+                </div>
+                <div className="about-description">
+
+                </div>
             </div>
         );
     }
