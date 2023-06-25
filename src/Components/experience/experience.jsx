a0a1d058f51c7e1cf231cb1910140dcc1d4407f2
JhoanVanDam <jhoansteam@gmail.com>
2023-06-24 21:00:12 -0500

    Arrrelgo de componente experiencia

--- src/Components/experience/experience.jsx
+++ src/Components/experience/experience.jsx
@@ -3,9 +3,25 @@ import './experience.css';
 class experience extends Component {
 
     render() {
-        const experienceItem = (date, ocupation, title, description, languages) => {
+        const Colors = [
+            'rgb(100, 0, 95)',
+            'rgb(32, 97, 83)',
+            'rgb(24, 73, 14)',
+            'rgb(35, 19, 109)',
+            'rgb(63, 68, 14)'
+        ];
+
+
+        const getColor = () => {
+            var posColors = Math.floor(Math.random() * (Colors.length));
+            return Colors[posColors];
+
+
+
+        }
+        const experienceItem = (date, ocupation, title, description, languages, color) => {
             return (
-                <div className='experience-item'>
+                <div className='experience-item' style={{ backgroundColor: color }}>
                     <span className='experience-item-date'>{date}</span>
                     <span className='experience-item-ocupation'>{ocupation}</span>
                     <span className='experience-item-title'>{title}</span>
@@ -21,15 +37,15 @@ class experience extends Component {
 
         const languages = ['JavaScript', 'HTML', 'CSS'];
 
-        
+
 
         return (
             <div className='experience-main' id='experiencia'>
-                {experienceItem('2022-2023', 'Software Developer', 'Project XYZ', 'Worked on frontend development', languages)}
-                {experienceItem('2022-2023', 'Software Developer', 'Project XYZ', 'Worked on frontend development', languages)}
-                {experienceItem('2022-2023', 'Software Developer', 'Project XYZ', 'Worked on frontend development', languages)}
-                {experienceItem('2022-2023', 'Software Developer', 'Project XYZ', 'Worked on frontend development', languages)}
-                {experienceItem('2022-2023', 'Software Developer', 'Project XYZ', 'Worked on frontend development', languages)}
+                {experienceItem('2022-2023', 'Desarrollador de software', 'Academusoft', 'Trabajé en el mantenimiento y desarrollo de las IES contratadas en el CIADTI', languages, getColor())}
+                {experienceItem('2022-2023', 'Software Developer', 'Project XYZ', 'Worked on frontend development', languages, getColor())}
+                {experienceItem('2022-2023', 'Software Developer', 'Project XYZ', 'Worked on frontend development', languages, getColor())}
+                {experienceItem('2022-2023', 'Software Developer', 'Project XYZ', 'Worked on frontend development', languages, getColor())}
+                {experienceItem('2022-2023', 'Software Developer', 'Project XYZ', 'Worked on frontend development', languages, getColor())}
             </div>
         );
     }
