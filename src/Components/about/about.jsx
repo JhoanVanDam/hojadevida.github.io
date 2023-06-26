ba44f31ede18c06315228832790a453264b0489b
JhoanVanDam <jhoansteam@gmail.com>
2023-06-26 11:01:46 -0500

    actualizacion about me

--- src/Components/about/about.jsx
+++ src/Components/about/about.jsx
@@ -6,17 +6,25 @@ class about extends Component {
         return (
             <div className="about-main" id="sobremi">
                 <img className="img-logo" src={jhoan} alt="logo" />
+                <h5>Sobre mí</h5>
                 <div className="about-description">
-                    <h5>Sobre mí</h5>
+                    <p>
+                        Mi nombre es Jhoan y soy un apasionado del aprendizaje y el crecimiento personal. Siempre busco oportunidades para ampliar mis conocimientos y desarrollar nuevas habilidades.
+                        Creo firmemente en la importancia de contribuir al crecimiento de las personas que me rodean al compartir conocimientos, experiencias y recursos. Siempre estoy dispuesto a ayudar en lo que sea necesario.
+                    </p>
                     <p>
                         Estudiante de Redes Y
                         Sistemas
                         Teleinformáticos.
-                        Me considero una
-                        persona responsable y
-                        ordenada, mis principales virtudes
-                        son la honestidad y el trabajo duro.
                     </p>
+                    <p>Mis principales virtues son</p>
+                    <ul className="about-description-list">
+                        <li>Puntualidad</li>
+                        <li>Honestidad</li>
+                        <li>Responsabilidad</li>
+                        <li>Empatía</li>
+                    </ul>
+
                 </div>
             </div>
 
