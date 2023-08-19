b02b7a1ba4f712622cb86c8346da38f21af53a56
JhoanVanDam <jhoansteam@gmail.com>
2023-08-19 10:57:05 -0500

    Agregando boton de descarga cv

--- src/Components/contact/contact.jsx
+++ src/Components/contact/contact.jsx
@@ -1,8 +1,19 @@
 import React, { Component } from 'react';
 import './contact.css';
 import emailjs from 'emailjs-com';
+import CV from './Source/JhoanCV2023.pdf';
+
 class contact extends Component {
     render() {
+        const downloadCV = () => {
+            const link = document.createElement('a');
+            link.href = CV;
+            link.download = 'JhoanCV2023.pdf';
+            link.style.display = 'none';
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+        };
 
         function enviarEmail(e) {
             e.preventDefault();
@@ -33,6 +44,11 @@ class contact extends Component {
                         <button type="submit" >Enviar</button>
                     </div>
                 </form>
+                <div className='contact-main-cv'>
+                    <h3>Descargar Curriculum</h3>
+                    <i class="fa-solid fa-file" />
+                    <button onClick={downloadCV}>Descargar Archivo</button>
+                </div>
             </div>
         );
     }
